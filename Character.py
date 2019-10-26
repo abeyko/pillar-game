cd274a4e5a0b7d547426c7cb6467094d4f74ceb2
Cynopolis <Cynopolis@users.noreply.github.com>
2019-10-26 11:55:58 -0500

    Update Character.py
    Added integration with the towerComm class

--- Character.py
+++ Character.py
@@ -1,41 +1,75 @@
 class Character:
-    position = [0, 0] # angle and LED row
+    position = [0, 0] # LED number and LED row
     color = [255, 255, 255]
     stepRatio = 96/23 # the ratio of motor steps to LED changes
     stepAngle = 0 # the angle of the stepper motor
-    #4 steps counter clockwise = one LED to the right
-    #23 LEDs and 96 steps counter clockwise = one revolution
-    def __init__(self):
+
+    # 4 steps counter clockwise = one LED to the right
+    # 23 LEDs and 96 steps clockwise = one revolution
+
+    # required a towerComm object to passed to it
+    def __init__(self, commClass):
+        self.comm = commClass
         return
 
-    #move the character to the right one led
+    # move the character to the right one led
     def moveRight(self):
+        self.updateTower((0,0,0)) # blank the previous LED
+        # makes sure you cant move to an LED over 300
         if(self.position[0] < 299):
             self.position[0] += 1
             self.calcStepPosition()
+            self.updateTower(self.color)
         return
 
-    #move the character to the left one led
+    # move the character to the left one led
     def moveLeft(self):
+        self.updateTower((0, 0, 0)) # blank the previous LED
+        # makes sure you cant move to an LED under 0
         if(self.position[0] > 0):
             self.position[0] += 1
             self.calcStepPosition()
+            self.updateTower(self.color)
         return
 
-    #move the character up one row
+    # move the character up one row
     def moveUp(self):
+        self.updateTower((0, 0, 0)) # blank the previous LED
+        # makes sure you cant move to a row above 2
         if(self.position[1] < 2):
             self.position[1] += 1
+            self.updateTower(self.color)
         return
 
-    #move the character down one row
+    # move the character down one row
     def moveDown(self):
-        if (self.position[1] > 0):
-            self.position[1] -= 1
+        self.updateTower((0, 0, 0)) # blank the previous LED
+
+        # makes sure you cant move to a row under 0
+        if (self.position[1] < 0):
+            self.position[1] += 1
+            self.updateTower(self.color)
+
         return
 
-    #calculate the posiiton of the stepper motor
+    # calculate the position of the stepper motor
     def calcStepPosition(self):
-        stepAngle = self.position[0]*self.stepRatio
-        LEDNum = self.position[0]
+        self.stepAngle = self.position[0]*self.stepRatio
+
+        # makes sure the step angle doesn't surpass 0-96
+        while(self.stepAngle > 96):
+            self.stepAngle -= 96
+        while (self.stepAngle < 0):
+            self.stepAngle += 96
+        return
+
+    # updates the tower with the new character position information
+    def updateTower(self, color):
+        self.comm.GoTo(self.stepAngle)
+        if(self.position[1] == 0):
+            self.comm.lowerBandLight(self.position[0], color)
+        elif (self.position[1] == 1):
+            self.comm.middleBandLight(self.position[0], color)
+        else:
+            self.comm.UpperBandLight(self.position[0], color)
         return
