af828068aea9ed8db6a7b4f4603d9f883c2f0ccb
Cynopolis <Cynopolis@users.noreply.github.com>
2019-10-26 12:23:45 -0500

    Update towerComm.py
    Simplified code

--- towerComm.py
+++ towerComm.py
@@ -35,20 +35,10 @@ class towerComm:
     # the color variable is a tuple like this: (r, g, b)
     def lowerBandLight(self, position, color):
         string = "Lx"
-        if(position < 10):
-            string += "00"+position
-        elif (position < 100):
-            string += "0" + position
-        elif (position > 100):
-            string += position
+        string += self.three_digit_str(position)
 
         for i in range(0, 2):
-            if (color[i] < 10):
-                string += "00" + color[i]
-            elif (color[i] < 100):
-                string += "0" + color[i]
-            elif (color[i] > 100):
-                string += color[i]
+            string += self.three_digit_str(color[i])
 
         string += "\n"
         self.ser.write(string.encode())
@@ -58,20 +48,10 @@ class towerComm:
     # the color variable is a tuple like this: (r, g, b)
     def middleBandLight(self, position, color):
         string = "Mx"
-        if(position < 10):
-            string += "00"+position
-        elif (position < 100):
-            string += "0" + position
-        elif (position > 100):
-            string += position
+        string += self.three_digit_str(position)
 
         for i in range(0, 2):
-            if (color[i] < 10):
-                string += "00" + color[i]
-            elif (color[i] < 100):
-                string += "0" + color[i]
-            elif (color[i] > 100):
-                string += color[i]
+            string += self.three_digit_str(color[i])
 
         string += "\n"
         self.ser.write(string.encode())
@@ -81,46 +61,11 @@ class towerComm:
     # the color variable is a tuple like this: (r, g, b)
     def upperBandLight(self, position, color):
         string = "Hx"
-        if(position < 10):
-            string += "00"+position
-        elif (position < 100):
-            string += "0" + position
-        elif (position > 100):
-            string += position
+        string += self.three_digit_str(position)
 
         for i in range(0, 2):
-            if (color[i] < 10):
-                string += "00" + color[i]
-            elif (color[i] < 100):
-                string += "0" + color[i]
-            elif (color[i] > 100):
-                string += color[i]
+            string += self.three_digit_str(color[i])
 
-        string += "\n"
-        self.ser.write(string.encode())
-        return# tells the tower to light up a certain led with a certain color in the lower band
-    # the color variable is a tuple like this: (r, g, b)
-    def lowerBandLight(self, position, color):
-        string = "Lx"
-        string += pos_color_str(self, position, color)
-        string += "\n"
-        self.ser.write(string.encode())
-        return
-
-    # tells the tower to light up a certain led with a certain color in the lower band
-    # the color variable is a tuple like this: (r, g, b)
-    def middleBandLight(self, position, color):
-        string = "Mx"
-        string += pos_color_str(self, position, color)
-        string += "\n"
-        self.ser.write(string.encode())
-        return
-
-    # tells the tower to light up a certain led with a certain color in the lower band
-    # the color variable is a tuple like this: (r, g, b)
-    def upperBandLight(self, position, color):
-        string = "Hx"
-        string += pos_color_str(self, position, color)
         string += "\n"
         self.ser.write(string.encode())
         return
