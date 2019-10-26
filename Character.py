76dbf56ac3f71655ca5081de0b7e1b49d189d5fe
Cynopolis <Cynopolis@users.noreply.github.com>
2019-10-26 10:53:12 -0500

    Created the character class
    The character class is responssible for keeping track of and controlling the position of the character on the tower. This should be the only class that can control the stepper motor since the tower should always be centered on the character.

--- Character.py
+++ Character.py
@@ -5,32 +5,36 @@ class Character:
     stepAngle = 0 # the angle of the stepper motor
     #4 steps counter clockwise = one LED to the right
     #23 LEDs and 96 steps counter clockwise = one revolution
-
     def __init__(self):
         return
 
+    #move the character to the right one led
     def moveRight(self):
         if(self.position[0] < 299):
             self.position[0] += 1
             self.calcStepPosition()
         return
 
+    #move the character to the left one led
     def moveLeft(self):
         if(self.position[0] > 0):
             self.position[0] += 1
             self.calcStepPosition()
         return
 
+    #move the character up one row
     def moveUp(self):
         if(self.position[1] < 2):
             self.position[1] += 1
         return
 
+    #move the character down one row
     def moveDown(self):
         if (self.position[1] > 0):
             self.position[1] -= 1
         return
 
+    #calculate the posiiton of the stepper motor
     def calcStepPosition(self):
         stepAngle = self.position[0]*self.stepRatio
         LEDNum = self.position[0]
