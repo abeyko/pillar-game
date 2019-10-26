d88dc057d61651e204ef5be961b84dcb45bb623c
Cynopolis <Cynopolis@users.noreply.github.com>
2019-10-26 11:34:24 -0500

    Tower Communications Update
    Added several more essential functions to the tower communications class.

--- towerComm.py
+++ towerComm.py
@@ -5,6 +5,7 @@ class towerComm:
         ser = serial.Serial('/dev/ttyACM0', 115200)
         return
 
+    # send anything you want
     def sendAnything(self, message):
         message += "\n"
         self.ser.write(message.encode())
@@ -78,5 +79,57 @@ class towerComm:
         self.ser.write(string.encode())
         return
 
+    # send the stop command
+    def stop(self):
+       self.ser.write("Stop\n".encode())
+       return
+
+    # tell tower to go to a position between 0 and 96
+    def GoTo(self, position):
+        message = "GoTo"
+        if(position > 96):
+            position -= 96
+        if (position < 0):
+            position += 96
+        if(position < 10):
+            message += "00" + position
+        else:
+            message += "0" + position
+
+        message += "\n"
+        self.ser.write(message.encode())
+        return
+
+    # move the tower clockwise by a certain increment
+    def CW(self, incriment):
+        message = "JogF"
+        if(incriment < 10):
+            message += "0" + incriment
+        else:
+            message += incriment
+        message += "\n"
+        self.ser.write(message.encode())
+        return
+
+    # move the tower counter clockwise by a certain increment
+    def CCW(self, incriment):
+        message = "JogR"
+        if(incriment < 10):
+            message += "0" + incriment
+        else:
+            message += incriment
+        message += "\n"
+        self.ser.write(message.encode())
+        return
+
+    # move the tower clockwise one step
+    def forward(self):
+        self.ser.write("For\n".encode())
+        return
+
+    # move the tower counter clockwise one step
+    def forward(self):
+        self.ser.write("Rev\n".encode())
+        return
 
 
